1b4656dbc3eb07be94786d297dd83ddf37ebbc85
pavass <41996522+pavass@users.noreply.github.com>
2021-03-31 00:20:02 +0530

    Create students.java

---
+++ students.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ADMIN
+ */
+public class Students {
+    public static void main(String[] args) {
+        int total=90;
+        int no_of_boys=45;
+        int total_secured=(90*50)/100;
+        int no_of_girls=total_secured-20;
+        System.out.println("Number of girls getting grade  A are "+no_of_girls);
+    }
+   
+}
