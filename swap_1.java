49cf7c267fad6b9a3cd43483d540e89e057c3689
pavass <41996522+pavass@users.noreply.github.com>
2021-03-31 00:17:26 +0530

    Create swap_1.java

---
+++ swap_1.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ADMIN
+ */
+public class Swap {
+    public static void main(String[] args) {
+        int a =6;
+        int b =8;
+        int temp =a;
+        a=b;
+        b=temp;
+        System.out.println("After swap:");
+        System.out.println("a : "+a);
+        System.out.println("b : "+b);
+    }
+}
