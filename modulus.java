3c77dc755a9d1aa63accd1552ed143c4db2d36c4
pavass <41996522+pavass@users.noreply.github.com>
2021-03-31 00:10:02 +0530

    Create modulus.java

---
+++ modulus.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ADMIN
+ */
+public class Modulus {
+    public static void main(String[] args) {
+        int num=2345;
+        int n  =(num+8)/3;
+        int m =(n%5)*5;
+        System.out.println("Result is: "+m);
+    }
+}
+© 2021 GitHub, Inc.
