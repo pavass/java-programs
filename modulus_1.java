b16daec8e2fd7fd2a8e69e383ed1634e701ace80
pavass <41996522+pavass@users.noreply.github.com>
2021-03-31 00:10:54 +0530

    Create modulus_1.java

---
+++ modulus_1.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ADMIN
+ */
+public class Modulus1 {
+    public static void main(String[] args) {
+        int num=2345;
+        num+=8;
+        num/=3;
+        num%=5;
+        num*=5;
+        System.out.println(num);
+    }
+}
