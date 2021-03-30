60d853181b474b8b83302aa516c45d75cfc7c90a
pavass <41996522+pavass@users.noreply.github.com>
2021-03-31 00:12:40 +0530

    Create power.java

---
+++ power.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ADMIN
+ */
+public class Power {
+    public static void main(String[] args) {
+        int a =7;
+       int power=(int)Math.pow(7,5);
+       System.out.println(power);
+    }
+}
