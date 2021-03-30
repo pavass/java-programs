09d7e0cde4f5f343f37c6bfd544900305de2f210
pavass <41996522+pavass@users.noreply.github.com>
2021-03-31 00:09:05 +0530

    Create perimeter_triangle.java

---
+++ perimeter_triangle.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ADMIN
+ */
+public class Perimeter_triangle {
+    public static void main(String[] args) {
+        int a=2;
+        int b =3;
+        int c =5;
+        System.out.println("Perimeter of trisngle =" +(a+b+c)+" units");
+    }
+}
