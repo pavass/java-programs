75a77eb2e9111a9245c521cea6b81f05fba27910
pavass <41996522+pavass@users.noreply.github.com>
2021-03-31 00:16:24 +0530

    Create marks.java

---
+++ marks.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ADMIN
+ */
+public class Marks {
+    public static void main(String[] args) {
+       int sub1=78;
+       int sub2=45;
+       int sub3=62;
+       double totalmarks=sub1+sub2+sub3;
+       double percent =(totalmarks/300)*100;
+       System.out.println("Total Marks= "+totalmarks);
+       System.out.println("Percentage Marks= "+percent);
+    }
+ 
+}
