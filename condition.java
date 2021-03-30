f132abd3443699f181674edbc05f6de555d7ecf6
pavass <41996522+pavass@users.noreply.github.com>
2021-03-31 00:15:07 +0530

    Create condition.java

---
+++ condition.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ADMIN
+ */
+public class Condition1 {
+    public static void main(String[] args) {
+        int a=55;
+        int b =70;
+        if(a<50 || a<b)
+        
+            System.out.println("Atleast one of the condition is true");
+        else 
+            System.out.println("Both are false");
+               
+    }
+}
