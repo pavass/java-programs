ca08917504151930d2026eca2219ec724290e6b0
pavass <41996522+pavass@users.noreply.github.com>
2021-03-31 00:11:52 +0530

    Create equal.java

---
+++ equal.java
@@ -0,0 +1,19 @@
+*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ADMIN
+ */
+public class Equal_check {
+    public static void main(String[] args) {
+        int a=23, b=45;
+        if(a==b)
+            System.out.println("Equal");
+        else
+            System.out.println("Not Equal");
+    }
+}
