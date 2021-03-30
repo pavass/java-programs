dc4fbbf48686a0803399e03db757dc1d057507d4
pavass <41996522+pavass@users.noreply.github.com>
2021-03-31 00:08:14 +0530

    Create perimeter_rectangle.java

---
+++ perimeter_rectangle.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ADMIN
+ */
+public class Perimeter_rectangle {
+    public static void main(String args[])
+    {
+        int l=5;
+        int b =7;
+        System.out.println("Perimeter= "+(2*(l+b)));
+        System.out.println("Area= "+(l*b));
+    }
+}
